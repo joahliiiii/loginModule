84030018a00e85d67530f0008e857a70f53858c1
joah <joahli@qq.com>
2017-09-07 12:51:41 +0800

    No message,just added something.

--- src/cn/joah/login/test/ExceptionTest.java
+++ src/cn/joah/login/test/ExceptionTest.java
@@ -24,7 +24,8 @@ public class ExceptionTest {
 
     public static void main(String[] args) {
         try {
-            int i = 10 / 0; // 抛出 Exception，后续处理被拒绝
+//            int i = 10 / 0; // 抛出 Exception，后续处理被拒绝
+            int i=1;
             System.out.println("i vaule is : " + i);
 //            return true;   // Exception 已经抛出，没有获得被执行的机会
         } catch (Exception e) {
@@ -32,7 +33,8 @@ public class ExceptionTest {
 //            return catchMethod();  // Exception 抛出，获得了调用方法的机会，但方法值在 finally 执行完后才返回
         }finally{
             finallyMethod();  // Exception 抛出，finally 代码块将在 catch 执行 return 之前被执行
+            System.out.println("helloooooo");
         }
-        System.out.println("helloooooo");
+//        System.out.println("helloooooo");
     }
 }
