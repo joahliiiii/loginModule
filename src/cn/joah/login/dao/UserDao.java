920b8044fd615e99c094aa0068acd8a03885c48d
joah <joahli@qq.com>
2017-09-09 20:15:45 +0800

    解决了及其sb的bug,就是把getColumnCount当成获取总行数来写 ...

--- src/cn/joah/login/dao/UserDao.java
+++ src/cn/joah/login/dao/UserDao.java
@@ -7,6 +7,7 @@ import com.mysql.jdbc.exceptions.MySQLIntegrityConstraintViolationException;
 
 import java.sql.Connection;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 
 /**
@@ -16,41 +17,36 @@ public class UserDao {
     /**
      * 使用用户名检索用户是否存在
      * @param uName
-     * @return 如果查询结果不为空,那么返回ture  否则返回false
+     * @return 如果不存在这个username就返回false
      */
     public boolean findByUsername(String uName) throws UserException {
         /**
          * 先获取链接
          * 写好sql
          * 调用selectInfo(conng,sql,args)
-         * 如果resultSet.getMetaData().getColumnCount() 为 1 那么不存在,否则存在
          */
         Connection connection= Utils.getConnection();
-        String sql="SELECT username FROM User WHERE username=? ";
+        String sql="SELECT count(*) FROM User WHERE username=? ";
         ResultSet resultSet=Utils.selectInfo(connection,sql,uName);
+
         try {
-            int count = resultSet.getMetaData().getColumnCount();
-            // 如果只有一行列名, 那么不存在, 返回false
-            if(count ==1) {
-                return false;
-            }/*else if(count == 2){
-                return true;
-            }*/
-        } catch (MySQLIntegrityConstraintViolationException e){
-            return true;
-//            throw new UserException("");
-//            e.printStackTrace();
-        }catch (SQLException e){
+            resultSet.next();
+            int value = resultSet.getInt(1);
+            // 得到第二行第一列的值
+            if(value != 0){
+                return  true;
+            }
+        } catch (SQLException e){
             e.printStackTrace();
         }
-        return true;
+        return false;
     }
 
     /**
      * 把一个用户对象插入到数据库内
      * @param user 用户对象
      */
-    public void insertUser(User user){
+    public void insertUser(User user)  {
         /**
          * 先获取连接
          * 写好sql
--- src/cn/joah/login/test/userDaoTest.java
+++ src/cn/joah/login/test/userDaoTest.java
@@ -2,17 +2,18 @@ package cn.joah.login.test;
 
 import cn.joah.login.dao.UserDao;
 import cn.joah.login.entity.User;
+import cn.joah.login.service.UserException;
 import org.junit.Test;
 
 public class userDaoTest {
     @Test
-    public void findByUsernameTest() {
+    public void findByUsernameTest() throws UserException {
         UserDao UserDao =new UserDao();
         boolean find = UserDao.findByUsername("joah");
         if(find){
             System.out.println("joah 在数据库内存在!");
         }else{
-            System.out.println("joah 在数据库内部存在!");
+            System.out.println("joah 在数据库内不存在!");
         }
     }
     @Test
