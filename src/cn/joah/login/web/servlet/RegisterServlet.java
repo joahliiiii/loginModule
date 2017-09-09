bdad6ab1430e2ed4a4bfa99c85dc543d8de5a0c1
joah <joahli@qq.com>
2017-09-09 16:19:12 +0800

    重写RegisterServlet

--- src/cn/joah/login/web/servlet/RegisterServlet.java
+++ src/cn/joah/login/web/servlet/RegisterServlet.java
@@ -1,68 +1,56 @@
 package cn.joah.login.web.servlet;
 
-import cn.joah.login.commons.jdbc.Utils;
+import cn.joah.login.commons.reflect.ReflectUtils;
+import cn.joah.login.entity.User;
+import cn.joah.login.service.UserException;
+import cn.joah.login.service.UserService;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.beans.IntrospectionException;
 import java.io.IOException;
-import java.sql.Connection;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Map;
 
 
 @WebServlet(name = "RegisterServlet", urlPatterns = "/cn/joah/login/web/servlet/RegisterServlet")
 public class RegisterServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//        response.getWriter().write("hello");
 
         // 先设置编码问题
         request.setCharacterEncoding("UTF-8");
         response.setCharacterEncoding("UTF-8");
+
         // 先校验验证码
         // 得到param参数的验证码
         String captche = request.getParameter("captche");
         // 得到session里面存储的验证吗
         String sCap = (String) request.getSession().getAttribute("captche");
-        // 如果param-captche 的值不为空,那么就开始校验
-        if(captche!=null){
-            // 如果相同 就吧数据存储到数据库内 ,重定向到登录页面
-            if(sCap.equalsIgnoreCase(captche)){
-                response.getWriter().write("hello data will transfer to database!");
-                /**
-                 * 需要新增加邮箱和profile的非法字符检测.
-                 * 先验证看看这里获取的参数是否全部正确,然后在进行下面的操作.
-                 *
-                 * 在这里需要吧密码先进行hash计算在吧所有数据存入数据库
-                 */
+        UserService userService=new UserService();
+        // 验证码正确
+        if(userService.checkCaptche(captche,sCap)){
 
-                    String uName =request.getParameter("username");
-                    String pass = request.getParameter("password");
-                    String sex = request.getParameter("sex");
-                    String email = request.getParameter("email");
-                    String website = request.getParameter("website");
-                    String profile = request.getParameter("profile");
-                    response.getWriter().write("\n"+"uName: "+uName+" pass: "+pass +" sex: "+sex+" email: "+email+" website:"+website
-                    +" profile: "+profile);
-                    String sql="insert into User(username,password,sex,email,website,profile) values(?,?,?,?,?,?)";
-//                    System.out.println("before connection!");
-//                    Utils utils=new Utils();
-                    Connection connection = Utils.getConnection();
+            Map<String, String[]> parameterMap = request.getParameterMap();
 
-                   /* System.out.println("connection: "+connection);
-                    response.getWriter().write("\n"+"connection: "+connection);*/
-                    Utils.updateInfo(connection,sql,uName, pass,sex,email,website,profile);
-//                    Utils.release(connection,null,null);
-//                    System.out.println("insert over!");
+            try {
+                User user = ReflectUtils.toBean(parameterMap, User.class);
 
-                    // 保存了之后重定向到登录界面
-                    response.sendRedirect("/jsps/login.jsp");
-
-                return;
-            }else{// 如果不相同的话就转发到注册页面
-                request.getRequestDispatcher("/jsps/regist.jsp").forward(request,response);
-//            return;
+                try {
+                    userService.register(user);
+                } catch (UserException e) {
+                    request.setAttribute("msg",e.getMessage());
+                    request.getRequestDispatcher("/jsps/regist.jsp").forward(request,response);
+                }
+            } catch (ClassNotFoundException | IllegalAccessException | InstantiationException | InvocationTargetException | NoSuchMethodException | IntrospectionException e) {
+                e.printStackTrace();
             }
-        }else{// 如果验证码为空
+            // 注册成功之后显示注册成功的信息, 并转发到登录界面
+            response.getWriter().write(" 注册成功! ");
+            response.sendRedirect("/jsps/login.jsp");
+
+        }else{// 如果验证码错误
             request.getRequestDispatcher("/jsps/regist.jsp").forward(request,response);
         }
     }
